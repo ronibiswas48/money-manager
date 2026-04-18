179d38f97279e6e337e76dfa72b66dea3892b1c2
MR <mr@MRs-Mac-mini.local>
2026-04-18 14:19:10 +0600

    Created Transaction model and schema

---
+++ models/Transaction.ts
@@ -0,0 +1,28 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const TransactionSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ['income', 'personal', 'medicine', 'family', 'savings'],
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps: true})
+
+export const Transaction = models.Transaction || model('Transaction', TransactionSchema) 
